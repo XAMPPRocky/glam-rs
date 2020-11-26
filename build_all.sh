44c7b7b00328e3c0c1225b678dd98b4039496707
Cameron Hart <cameron.hart@gmail.com>
2020-11-26 23:42:57 +1300

    Move all vec impls to macros in preparation for other types.

--- build_all.sh
+++ build_all.sh
@@ -2,4 +2,5 @@
 
 cargo test --features "bytemuck mint rand serde debug-glam-assert transform-types" && \
 cargo test --features "scalar-math bytemuck mint rand serde debug-glam-assert transform-types" && \
-cargo test --no-default-features --features "libm scalar-math bytemuck mint rand serde debug-glam-assert transform-types"
+cargo test --no-default-features --features "libm scalar-math bytemuck mint rand serde debug-glam-assert transform-types" && \
+cargo bench --no-run
